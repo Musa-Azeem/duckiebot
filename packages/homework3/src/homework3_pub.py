3c8e90c7c0c11d86218e9e7fdb77673da3ce8dc6
Musa Azeem <mmazeem@email.sc.edu>
2021-09-28 17:07:13 -0400

    Homework3

--- packages/homework3/src/homework3_pub.py
+++ packages/homework3/src/homework3_pub.py
@@ -5,14 +5,6 @@ import random
 
 from std_msgs.msg import Float32
 
-#class Homework3Pub:
-#    def __init__(self):
-#        #rospy.Subscriber('input', Float32, self.callback)
-#        self.pub = rospy.Publisher("delta", Float32, queue_size = 10)
-#
-#    def callback(self, data):
-#        self.pub.publish(data.data)
-
 def publish_value():
         pub = rospy.Publisher("delta", Float32, queue_size = 10)
         rospy.init_node('homework3')
@@ -23,8 +15,6 @@ def publish_value():
 
 if __name__ == '__main__':
     try:
-        #Homework3Pub()
-        #rospy.spin()
         publish_value()
     except rospy.ROSInterruptException:
         pass
