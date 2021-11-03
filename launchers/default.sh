eaa5dddb1009b5b5b9982d375f707641e3f297f0
Musa Azeem <mmazeem@email.sc.edu>
2021-11-02 22:59:32 -0400

    tmux and vim config

--- launchers/default.sh
+++ launchers/default.sh
@@ -7,7 +7,19 @@ dt-launchfile-init
 
 # YOUR CODE BELOW THIS LINE
 # ----------------------------------------------------------------------------
+touch ~/.vimrc
+cat "set number" >> ~/.vimrc
+cat "set nowrap" >> ~/.vimrc
 
+touch ~/.tmux.conf
+cat "unbind C-b" >> ~/.tmux.conf
+cat "set -g prefix C-a" >> ~/.tmux.conf
+cat "bind C-a send-prefix" >> ~/.tmux.conf
+cat "set -g mouse" >> ~/.tmux.conf
+cat "bind \\ split-window -h"  >> ~/.tmux.conf
+cat "bind - split-window -v" >> ~/.tmux.conf
+cat "unbind \"" >> ~/.tmux.conf
+cat "unbind %" >> ~/.tmux.conf
 
 # NOTE: Use the variable DT_REPO_PATH to know the absolute path to your code
 # NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
