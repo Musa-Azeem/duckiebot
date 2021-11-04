92fe05f78c84a40f8aa268c3421365578ef3b63e
MuhammadTukhtasunov <muhammadtukhtasunov@gmail.com>
2021-11-04 12:51:27 -0400

    fixed indent error #2

--- packages/project3/src/lane_following_p3.py
+++ packages/project3/src/lane_following_p3.py
@@ -26,7 +26,7 @@ class lane_follow:
         rospy.on_shutdown(self.stop)
 
         #self.v = 1.5     #constant velocity during lane following
-        self.v = int(rospy.get_param('v', '0.3')) #LOOK HERE
+        self.v = int(rospy.get_param('v', '0.3'))
         #self.kp = 1.3     #propertional gain
         #self.kd = 1     #derivative gain
         self.kp = int(rospy.get_param('kp', '1'))
@@ -79,7 +79,7 @@ class lane_follow:
             Sets v and omega of next action message and publishes to cmd topic
             Updates last time to current time for next callback
         """
-	rospy.loginfo('in callback')
+	rospy.loginfo("in callback")
         #find delta time
         curr_time = rospy.Time.now().to_sec()
         if self.last_time is not None:
