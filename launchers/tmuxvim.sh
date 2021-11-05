bdf246f22f9374b49d11e3d4ae26e7a7103005e2
Musa Azeem <mmazeem@email.sc.edu>
2021-11-04 21:18:26 -0400

    launchers

---
+++ launchers/tmuxvim.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#.vimrc
+cat << EOF > ~/.vimrc
+set number
+set nowrap
+EOF
+
+#.tmux.conf
+cat << EOF > ~/.tmux.conf
+#change ctrl-B to ctrl-A
+unbind C-b 
+set -g prefix C-a 
+bind C-a send-prefix
+
+#set mouse
+set -g mouse
+
+bind \\ split-window -h
+bind - split-window -v
+unbind '"' 
+unbind %
+EOF
